4a6768c7ed91d2b11bac173b6f89bcacb8b2ea64
Artem <ar.kalinin@bars.group>
2024-03-12 11:25:32 +0300

    main: вертска навигации слайдера

--- src/js/components/artemMainJS/artem-main.js
+++ src/js/components/artemMainJS/artem-main.js
@@ -64,10 +64,18 @@ const initCaruselsSectionSuitesTable = () => {
   // Инициализация всех слайдеров у которых класс "owl-carousel"
   for (let i = 0; i < owlCarousel.length; i++) {
     $(owlCarousel[i]).owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      lazyLoad:true,
+      autoplay: true,
+      rewind: true, /* use rewind if you don't want loop */
+      margin: 20,
+       /*
+      animateOut: 'fadeOut',
+      animateIn: 'fadeIn',
+      */
+      responsiveClass: true,
+      autoHeight: true,
+      autoplayTimeout: 7000,
+      smartSpeed: 800,
+      nav: true,
       responsive:{
           0:{
               items:1
